f24b2aa8eadae662ce7ba8472349d17620e31fe3
Nicolas Tessore <n.tessore@ucl.ac.uk>
2023-01-30 15:47:55 +0000

    MAINT: update for glass 2023.1

--- glass/extension.py
+++ glass/extension.py
@@ -2,34 +2,8 @@
 # license: MIT
 '''GLASS module for extension'''
 
-__version__ = '2022.10.6'
+__version__ = '2023.1'
 
 
-# non-GLASS imports
-import logging
-
-from glass.generator import generator
-from glass.cosmology import ZMIN, ZMAX
-# ... other GLASS imports (don't forget to add requirements if extensions)
-
-logger = logging.getLogger(__name__)
-
-# variable definitions
-ZMEAN = 'mean redshift of matter shell'
-'''An example variable for the mean redshift of the matter shell.'''
-
-
-@generator(
-    receives=(ZMIN, ZMAX),
-    yields=ZMEAN)
-def zmean():
-    '''example generator for the mean redshift'''
-
-    zmean = None
-
-    while True:
-        zmin, zmax = yield zmean
-
-        zmean = (zmin + zmax)/2
-
-        logger.info('mean redshift: %.3f', zmean)
+# your code here
+...
